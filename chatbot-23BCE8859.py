f63f763ab14967f4c66a77225f26b148c3dd945c
Kotha-Amrutha01 <150663353+Kotha-Amrutha01@users.noreply.github.com>
2023-12-20 00:10:57 +0530

    Add files via upload

---
+++ chatbot-23BCE8859.py
@@ -0,0 +1,94 @@
+a=input("Enter your name:")
+print("Hi",a,"I'm ur virtual frnd","\nHow are you?")
+c=input(">")# fine or not fine#
+if('not fine'in c or 'not well'in c or 'not ok' in c or 'not good' in c):
+     print("I'm really sorry to hear that...")
+     x=input("would you like me to suggest some songs for u?\n")
+     if("yes" in x):
+         print("what kind of songs would you like me to suggest you?")
+         b=input("can you please suggest me some ")
+         if("feel good songs" in b):
+            print("apna bana le-bhediya")
+            print("tum hi ho-aashique2")
+            print("jeene laga hoon-ramaiya vastavaiya")
+            print("tera ban jaunga-kabhir singh")
+            print("ee rathalee-radhe shyam")
+            print("kalalo kooda-liger")
+            print("kumkumala-bramastra")
+            print("hoyna hoyna-gang leader")
+         elif("beat songs" in b):
+             print("dholida-loveyatri")
+             print("dhol bhaje-ramleela")
+             print("kaala chashma-bhar bhar dheko")
+             print("badtameez dil-yeh jawani yeh dewani")
+             print("dimak karabhe-ismart shankar")
+             print("top lesipoddi-idharu ammayilatho")
+             print("ninnu road medha -savyasachi")
+             print("rowdy baby-maari2")
+         elif("breakup songs"in b):
+             print("agar tum sath ho-tamaasha")
+             print("dil diyan gallan-tiger zindha hai")
+             print("channa mereya-ADHM")
+             print("bhekayali-kabhir singh")
+             print("vintunnava-ye maaya chesave")
+             print("yetu pone-arjun reddy")
+             print("my love is gone-arya2")
+             print("nee kallalone-jai lava kusa")
+         elif("private songs" in b):
+             print("vaste-dhvani bhanushali")
+             print("raanjhana-Arjith singh")
+             print("mahiye jinna soona-darshana raval")
+             print("dheere dheere-yo yo honet singh")
+             print("oh pilla-haricharan")
+             print("vellake-anirudh ravichander")
+             print("thattukoede-vijai bulganin")
+             print("arupu-rollrida")
+         elif("devotional song" in b):
+             print("namo namo ji shankara-kedarnath")
+             print("deva sri ganesh-agneepath")
+             print("tum prem ho-mohit lalwani")
+             print("shri krishna govind hare murari-mohit lalwani")
+             print("omkareswari-badrenath")
+             print("om namo venkatesaya-om namo venkatesaya")
+             print("madhura madhura meenakshi-Arjun")
+             print("mahishasura mardhini-shakthi")
+         elif("english songs" in b):
+            print("gasolina-daddy yankee")
+            print("unstoppable-sia")
+            print("safari-serena")
+            print("baby-justin bieber")
+            print("love me like you do-ellie goulding")
+            print("no lia-dua lipa")
+            print("lover-talyor swift")
+            print("who says-selena gomez")
+         elif("bts" in b):
+            print("fire")
+            print("dynamite")
+            print("mic drop")
+            print("we are bulletproof eternal")
+            print("fake love")
+            print("life goes on")
+            print("make it right")
+            print("magic shop")
+         else:
+            pass
+     else:
+            print("It's ok then Try to visit some peaceful place or try to talk with your parents,family and friends or try to go outside and play games to refresh your mind")
+else:
+      print("That's great to hear!If there's anything you'd like to chat about or if you have ant questions,feel free to let ne know.I'm here for you")
+      d=input("I think you are having a great time today\n")
+      if("yes" in d):
+          print("To make your day some more beautiful and joyful listen your favourite song")
+      else:
+        pass
+y=input(">")
+if ('thank you' in y or 'tq in y' or 'thanks in y'):
+      print("you are welcome")
+print("i'm hope you enjoyed this mini version of chat bot!")
+print("you can chat with me any time")
+            
+
+
+    
+
+         
